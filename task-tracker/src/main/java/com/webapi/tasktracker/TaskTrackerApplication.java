32cf2d13d6339b6ae18a29630d521e6a402a78fe
Maithilee Vaidya <32967517+maithileev@users.noreply.github.com>
2020-08-27 09:46:11 +0530

    First Commit

--- task-tracker/src/main/java/com/webapi/tasktracker/TaskTrackerApplication.java
+++ task-tracker/src/main/java/com/webapi/tasktracker/TaskTrackerApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaskTrackerApplication.class, args);
-		
+		SpringApplication.run(TaskTrackerApplication.class, args);		
 	}
 
 }
